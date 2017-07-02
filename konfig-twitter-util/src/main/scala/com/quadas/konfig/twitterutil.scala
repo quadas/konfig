0cfe05ccf3c6ea9ad173365fc95f90a70355deb8
Yan Su <yan.su@quadas.com>
2017-07-02 09:27:11 +0800

    have a twitter util readers trait

--- konfig-twitter-util/src/main/scala/com/quadas/konfig/twitterutil.scala
+++ konfig-twitter-util/src/main/scala/com/quadas/konfig/twitterutil.scala
@@ -3,12 +3,14 @@ package com.quadas.konfig
 import com.twitter.util.{ Duration, StorageUnit }
 import com.typesafe.config.Config
 
-package object twitterutil {
-  implicit object durationReader extends ConfigReader[Duration] {
-    override def read(c: Config, path: String) = Duration.fromNanoseconds(c.getDuration(path).toNanos)
+trait TwitterUtilReaders {
+  implicit val durationReader = new ConfigReader[Duration] {
+    override def read(c: Config, path: String): Duration = Duration.fromNanoseconds(c.getDuration(path).toNanos)
   }
 
-  implicit object storageUnitReader extends ConfigReader[StorageUnit] {
-    override def read(c: Config, path: String) = StorageUnit.fromBytes(c.getBytes(path))
+  implicit val storageUnitReader = new ConfigReader[StorageUnit] {
+    override def read(c: Config, path: String): StorageUnit = StorageUnit.fromBytes(c.getBytes(path))
   }
 }
+
+package object twitterutil extends TwitterUtilReaders
